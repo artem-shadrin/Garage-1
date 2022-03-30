996a38aff0efdccc90594abd5c779dbb071bb620
Neotwalker <and1marat9@gmail.com>
2022-03-31 02:30:50 +0500

    добавил боковое меню

---
+++ js/components/admin.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+  $('.button--create').on('click', function(e) {
+    e.preventDefault();
+    $('.create').toggleClass('create_active');
+    $('.header').toggleClass('header_active');
+    if ($(this).hasClass('visible')) {
+      $(this).removeClass('visible');
+      $(this).toggleClass('active');
+      $(this).css("pointer-events", "auto");
+      $('.button--create').css("pointer-events", "none");
+    } else {
+      $(this).addClass('visible');
+      $(this).removeClass('active');
+      $('.button--create').css("pointer-events", "auto");
+    };
+  });
+  $('.create__button').on('click', function(e) {
+    e.preventDefault();
+    $('.create').removeClass('create_active');
+    $('.header').removeClass('header_active');
+  });
+});
