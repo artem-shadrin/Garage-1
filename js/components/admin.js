c19042fb67e94115a7ee833226a609c98970626c
Neotwalker <and1marat9@gmail.com>
2022-03-31 14:42:04 +0500

    добавил страницы чата

--- js/components/admin.js
+++ js/components/admin.js
@@ -3,23 +3,40 @@ $(document).ready(function() {
     e.preventDefault();
     $('.create').toggleClass('create_active');
     $('.header').toggleClass('header_active');
-    if ($(this).hasClass('visible')) {
-      $(this).removeClass('visible');
-      $(this).toggleClass('active');
-      $(this).css("pointer-events", "auto");
-      $('.button--create').css("pointer-events", "none");
-    } else {
-      $(this).addClass('visible');
-      $(this).removeClass('active');
-      $('.button--create').css("pointer-events", "auto");
-    };
   });
-  $('.create__button').on('click', function(e) {
+
+
+  $('.button--add__news').on('click', function(e) {
+    e.preventDefault();
+    $('.create__add').toggleClass('create_active');
+    $('.header').toggleClass('header_active');
+  });
+
+
+  $('.button--add').on('click', function(e) {
+    e.preventDefault();
+    $('.create__survey').toggleClass('create_active');
+    $('.header').toggleClass('header_active');
+  });
+
+  
+  $('.create .create__button').on('click', function(e) {
     e.preventDefault();
     $('.create').removeClass('create_active');
     $('.header').removeClass('header_active');
-    $('.button--create').css("pointer-events", "auto");
-    $('.button--create').addClass('visible');
-    $('.button--create').removeClass('active');
+  });
+
+
+  $('.create__add .create__button').on('click', function(e) {
+    e.preventDefault();
+    $('.create__add').removeClass('create_active');
+    $('.header').removeClass('header_active');
+  });
+
+  
+  $('.create__survey .create__button').on('click', function(e) {
+    e.preventDefault();
+    $('.create__survey').removeClass('create_active');
+    $('.header').removeClass('header_active');
   });
 });
